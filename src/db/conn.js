dc4423601f19fa3f8b34f80e55ec7821c0bbb53b
Triben <tribenchoudhary1@gmail.com>
2021-10-21 18:20:48 +0530

    db corrected

--- src/db/conn.js
+++ src/db/conn.js
@@ -3,7 +3,8 @@ const DB = process.env.DATABASE;
 mongoose.connect(DB,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
-    useCreateIndex:true
+    useCreateIndex:true,
+    useFindAndModify:false
 }).then(()=>{
     console.log("Database Connected");
 }).catch((err)=>{
